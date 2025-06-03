af26cd79cbacdd5204a7d59572dd638e57fa5800
David Igbo <davidokpah396@gmail.com>
2025-06-03 21:43:31 +0100

    add profile model and working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
  root :to => "home#index"
+
+ namespace :admin do
+  resources :users 
+  root "dashboard#index"
+
+  resources :profile, only: %i[show edit update]
 end
