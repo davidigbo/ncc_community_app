4f5fe9c8c09fd1d034481803c2aa13dcb5445aa4
David Igbo <davidokpah396@gmail.com>
2025-06-09 05:24:31 +0100

    update

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,8 +4,11 @@ class Admin::DashboardController < ApplicationController
 
   def index
     @users_count = User.count
-    @recent_users = User.order(created_at: :desc).limit(10) # latest 10 users
+    @recent_users = User.order(created_at: :desc).limit(10) 
 
+    @user =User.find(params[:user_id]) if params[:user_id].present?
+    @user = User.first
+  
     @business_profiles_pending = BusinessProfile.pending.count
     @business_profiles_approved = BusinessProfile.approved.count
     @business_profiles_rejected = BusinessProfile.rejected.count
